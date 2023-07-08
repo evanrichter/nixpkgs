74257c546a55cb23f60bf7bd725e26572927ea6b
Evan Richter <evanjrichter@gmail.com>
2023-07-08 17:45:11 -0500

    oneVPL-intel-gpu: init at 23.3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24298,6 +24298,8 @@ with pkgs;
 
   oneVPL = callPackage ../development/libraries/oneVPL { };
 
+  oneVPL-intel-gpu = callPackage ../development/libraries/oneVPL-intel-gpu { };
+
   oniguruma = callPackage ../development/libraries/oniguruma { };
 
   oobicpl = callPackage ../development/libraries/science/biology/oobicpl { };
